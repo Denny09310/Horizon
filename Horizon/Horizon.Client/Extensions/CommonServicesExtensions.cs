33f9d7b9f4c3391ca1098ad80f0c7033949c1f10
Denny09310 <k.denny2000@gmail.com>
2024-12-18 22:36:46 +0100

    feat: added Radzen components library

---
+++ Horizon/Horizon.Client/Extensions/CommonServicesExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Radzen;
+
+namespace Horizon.Client.Extensions;
+
+public static class CommonServicesExtensions
+{
+    public static IServiceCollection AddCommonServices(this IServiceCollection services)
+    {
+        services.AddRadzenComponents();
+        return services;
+    }
+}
--- Horizon/Horizon.Client/Program.cs
+++ Horizon/Horizon.Client/Program.cs
@@ -2,4 +2,6 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
+builder.Services.AddCommonServices();
+
 await builder.Build().RunAsync();
